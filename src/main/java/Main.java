90e61240e0a064c66eab83466b001585a925adfe
mhrimaz <mhrimaz@yahoo.com>
2019-02-10 09:12:48 +0330

    second init

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,14 @@
+import org.apache.log4j.BasicConfigurator;
+
 import static spark.Spark.*;
 
 public class Main {
-
+    static {
+        BasicConfigurator.configure();
+    }
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
-        get("/hello", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
+        get("report.svg?url=master", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
     }
 
     static int getHerokuAssignedPort() {
