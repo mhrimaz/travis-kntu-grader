a79150d8ea4e2472bb1233aadd0ffb333c18c12a
mhrimaz <mhrimaz@yahoo.com>
2019-02-10 18:45:08 +0330

    why i am not working?

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,18 @@ import org.apache.commons.io.IOUtils;
 import org.apache.log4j.BasicConfigurator;
 
 import java.io.InputStream;
+import java.awt.Rectangle;
+import java.awt.Graphics2D;
+import java.awt.Color;
+import java.io.Writer;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
 
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.dom.GenericDOMImplementation;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.DOMImplementation;
 import static spark.Spark.*;
 
 public class Main {
@@ -11,21 +22,38 @@ public class Main {
         BasicConfigurator.configure();
 
     }
-
+    public static void paint(Graphics2D g2d) {
+        g2d.setPaint(Color.red);
+        g2d.fill(new Rectangle(10, 10, 100, 100));
+    }
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> {
-            res.header("Content-Encoding", "gzip");
-            res.header("Content-Type", "image/svg+xml");
-            res.header("Cache-Control","max-age=300");
-            res.status(200);
+//            res.header("Content-Encoding", "gzip");
+//            res.header("Content-Type", "image/svg+xml");
+//            res.header("Cache-Control","max-age=300");
+//            res.status(200);
             res.type("image/svg+xml");
-            return "<?xml version=\"1.0\" standalone=\"no\"?>\n" +
-                    "<!DOCTYPE svg PUBLIC \"-//W3C//DTD SVG 1.1//EN\" \n" +
-                    "\"http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\">\n" +
-                    "<svg width=\"100%\" height=\"100%\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">\n" +
-                    "<circle cx=\"100\" cy=\"50\" r=\"40\" stroke=\"black\" stroke-width=\"2\" fill=\"red\"/>\n" +
-                    "</svg>";
+            // Get a DOMImplementation.
+            DOMImplementation domImpl =
+                    GenericDOMImplementation.getDOMImplementation();
+
+            // Create an instance of org.w3c.dom.Document.
+            String svgNS = "http://www.w3.org/2000/svg";
+            Document document = domImpl.createDocument(svgNS, "svg", null);
+
+            // Create an instance of the SVG Generator.
+            SVGGraphics2D svgGenerator = new SVGGraphics2D(document);
+
+            // Ask the test to render into the SVG Graphics2D implementation.
+
+            paint(svgGenerator);
+
+            // Finally, stream out SVG to the standard output using
+            // UTF-8 encoding.
+            boolean useCSS = true; // we want to use CSS style attributes
+            svgGenerator.stream(res.raw().getWriter(), useCSS);
+            return res;
         });
 
         get("report.svg" , (req, res) -> {
