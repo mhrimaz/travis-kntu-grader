eb621cdd07892b7e7ac6b35097809f2fdd2b053a
mhrimaz <mhrimaz@yahoo.com>
2019-02-10 09:33:51 +0330

    route fix

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -10,7 +10,9 @@ public class Main {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
 
-        get("report.svg?url=master", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
+        get("report.svg" +
+
+                "", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
     }
 
     static int getHerokuAssignedPort() {
