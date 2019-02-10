05e18059b190a3c9bc4c9b7d0bbc57c2d43bec85
mhrimaz <mhrimaz@yahoo.com>
2019-02-10 09:42:07 +0330

    fix content type

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,13 +6,15 @@ public class Main {
     static {
         BasicConfigurator.configure();
     }
+
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
         get("/", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
 
-        get("report.svg" +
-
-                "", (req, res) -> "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>");
+        get("report.svg" , (req, res) -> {
+            res.type("image/svg+xml;charset=utf-8");
+            return "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"100\" height=\"100%\"> <circle cx=\"50\" cy=\"50\" r=\"30\" fill=\"red\"> </svg>";
+        });
     }
 
     static int getHerokuAssignedPort() {
